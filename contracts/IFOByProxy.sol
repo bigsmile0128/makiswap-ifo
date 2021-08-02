6846ff476c7a9ae367a4dc3fd20e03ae2109d75c
charisma-smile <goldwebnike@gmail.com>
2021-08-01 22:25:54 -0400

    feat: ifo latest update

--- contracts/IFOByProxy.sol
+++ contracts/IFOByProxy.sol
@@ -4,7 +4,7 @@ import '@openzeppelin/contracts/utils/math/SafeMath.sol';
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 import '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
 import '@openzeppelin/contracts/security/ReentrancyGuard.sol';
-import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts/utils/Initializable.sol";
 
 contract IFOByProxy is ReentrancyGuard, Initializable {
   using SafeMath for uint256;
